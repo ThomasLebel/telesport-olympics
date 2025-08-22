eeefb248cfbfec3a84043e66ae018312afed2894
Thomas Lebel <thomas.lebel38@gmail.com>
2025-08-22 16:53:54 +0200

    prep données diagram

---
+++ src/app/core/models/DiagramData.ts
@@ -0,0 +1,4 @@
+export interface DiagramData {
+  country: string;
+  nbMedals: number;
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { DiagramData } from 'src/app/core/models/DiagramData';
 import { Olympic } from 'src/app/core/models/Olympic';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
@@ -12,6 +13,7 @@ export class HomeComponent implements OnInit {
   public olympics$: Observable<Olympic[] | null> = of(null);
   public nbJOs: number = 0;
   public nbCountries: number = 0;
+  public diagramData: DiagramData[] = [];
 
   constructor(private olympicService: OlympicService) {}
 
@@ -21,7 +23,23 @@ export class HomeComponent implements OnInit {
       if (data) {
         this.nbCountries = data.length;
         this.nbJOs = data[0].participations.length;
+        this.diagramData = this.initDiagramData(data);
       }
     });
+  }
+
+  initDiagramData(data: Olympic[]): DiagramData[] {
+    let result = [];
+    for (let country of data) {
+      let nbMedals = 0;
+      for (let participation of country.participations) {
+        nbMedals += participation.medalsCount;
+      }
+      result.push({
+        country: country.country,
+        nbMedals,
+      });
+    }
+    return result;
   }
 }
